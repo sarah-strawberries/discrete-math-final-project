0314896f0595b75257e7f8ba814fe7a026b31f8d
Sarah Martin <blue.howrse@gmail.com>
2024-04-26 10:28:02 -0600

    Added code to get integer input from user

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace discrete_math_final_project;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string userInput;
+        int n = 0;
+        Console.WriteLine("Please enter a nonzero integer to be factored.");
+        userInput = Console.ReadLine();
+
+        while (n == 0)
+        {
+            try
+            {
+                n = Int32.Parse(userInput);
+                if (n == 0)
+                {
+                    Console.WriteLine("Input integer must not be 0. Please try again.");
+                    userInput = Console.ReadLine();
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Input must be an integer. Please try again.");
+                userInput = Console.ReadLine();
+            }
+        }
+    }
+}
