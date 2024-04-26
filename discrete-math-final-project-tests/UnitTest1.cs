e2c1f4ce8004d34f307267382f9437df41de5107
Sarah Martin <blue.howrse@gmail.com>
2024-04-26 01:13:26 -0600

    Added nunit test project

---
+++ discrete-math-final-project-tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using discrete_math_final_project;
+namespace discrete_math_final_project_tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
