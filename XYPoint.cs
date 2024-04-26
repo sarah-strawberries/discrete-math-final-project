9ad36a26c3528244ab2699a9badea545b1ddb104
Sarah Martin <blue.howrse@gmail.com>
2024-04-25 23:30:59 -0600

    Added constructor and properties to XYPoint

--- XYPoint.cs
+++ XYPoint.cs
@@ -9,12 +9,20 @@ namespace discrete_math_final_project
     internal class XYPoint
     {
         #region Constructor
+        public XYPoint(double x, double y)
+        {
+            this.X = x; 
+            this.Y = y;
+        }
         #endregion
 
         #region Properties
+        public double X { get; set; }
+        public double Y { get; set; }
         #endregion
 
         #region Methods
+
         #endregion
     }
 }
